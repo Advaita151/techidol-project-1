ad9a055a3df0b3e9b0fe1fb66a9bc9bf0db4cc43
Advaita <vedanta3000@gmail.com>
2023-11-06 19:16:51 +0530

    created contact us page

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,25 @@ import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from "cors";
 import contactUsRoute from './routers/contactUsRoute.js'
+import path from 'path'
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT
 const mongoDB = process.env.MONGODB_URI
 
+
 const mongo = "mongodb://localhost:27017/techidol1"
 
 app.use(express.json());
 app.use(cors());
+// app.use(express.static('public'));
 
 app.get("/",(req,res)=>{
     res.status(234).send("WELCOME to my website")
 })
+
+
 
 app.use("/contact",contactUsRoute);
 
